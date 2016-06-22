f4e3b670180ddeff6e64eb66b7b601d6e1bbce4d
张龙 <zhanglongit@126.com>
2016-06-23 07:45:48 +0800

    log

--- app/src/main/java/cin/hello/com/myapplication/MainActivity.java
+++ app/src/main/java/cin/hello/com/myapplication/MainActivity.java
@@ -1,13 +1,17 @@
 package cin.hello.com.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.orhanobut.logger.Logger;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final String TAG="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Logger.init(TAG);
+        Logger.d("zhanglong","--->onCreate()");
     }
 }
